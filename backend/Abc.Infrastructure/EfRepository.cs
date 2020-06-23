3de0417fa6e40dbf69daf76821d43dea55e55970
Muhammed Ali <40212997+muhammedalibalci@users.noreply.github.com>
2020-06-23 13:10:56 +0300

    changed struct of folder

---
+++ backend/Abc.Infrastructure/EfRepository.cs
@@ -0,0 +1,72 @@
+﻿using Abc.Core.Entities;
+using Abc.Core.Interfaces;
+using Abc.Core.Specifications;
+using Abc.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abc.Infrastructure
+{
+    public class EfRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly EfDbContext _context;
+        public EfRepository(EfDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetEntityWithSpec(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).FirstOrDefaultAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).ToListAsync();
+        }
+        public async Task<int> CounAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).CountAsync();
+        }
+
+        private IQueryable<T> ApplySpecification(ISpecification<T> spec)
+        {
+            return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
+        }
+
+        public async Task<T> Add(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Added;
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<T> Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<T> Delete(T entity)
+        {
+            _context.Remove(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
