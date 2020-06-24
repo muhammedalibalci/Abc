417791eb283b66727f446271d7c883a945b55c75
Muhammed Ali <40212997+muhammedalibalci@users.noreply.github.com>
2020-06-24 17:40:25 +0300

    deleted client

--- client/index.js
+++
@@ -1,7 +0,0 @@
-
-import {AppRegistry} from 'react-native';
-import Login from './src/screens/Login';
-import Router from './src/router/Router';
-import {name as appName} from './app.json';
-console.disableYellowBox = true;
-AppRegistry.registerComponent(appName, () => Router);
--- client/src/api/postApi.js
+++
@@ -1,7 +0,0 @@
-import Axios from "axios"
-
-const url = "https://abc-app-api.azurewebsites.net/api"
-
-export const getPosts = () => {
-    return Axios.get(url + '/posts')
-}
